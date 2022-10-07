7bb6a4dbf68912afa906ef89c169fc6c9704f245
Shivam Rana <89553377+skr20wdt@users.noreply.github.com>
2022-10-06 22:01:59 -0500

    the three routes according to our database design

---
+++ backend/organizationData.js
@@ -0,0 +1,109 @@
+const express = require("express"); 
+const router = express.Router(); 
+
+//importing data model schemas
+let { organizationdata } = require("../models/models"); 
+
+//GET all entries
+router.get("/", (req, res, next) => { 
+    organizationdata.find( 
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    ).sort({ 'updatedAt': -1 }).limit(10);
+});
+
+//GET single entry by ID
+router.get("/id/:id", (req, res, next) => {
+    organizationdata.find( 
+        { _id: req.params.id }, 
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+//GET entries based on search query
+//Ex: '...?firstName=Bob&lastName=&searchBy=name' 
+router.get("/search/", (req, res, next) => { 
+    let dbQuery = "";
+    if (req.query["searchBy"] === 'orgname') {
+        dbQuery = { orgName: { $regex: `^${req.query["orgName"]}`, $options: "i" }}
+    } else if (req.query["searchBy"] === 'number') {
+        dbQuery = {
+            "orgPhone.primaryPhone": { $regex: `^${req.query["orgPhone.primaryPhone"]}`, $options: "i" }
+        }
+    };
+    primarydata.find( 
+        dbQuery, 
+        (error, data) => { 
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+//GET events for a single client
+router.get("/events/:id", (req, res, next) => { 
+    
+});
+
+//POST
+router.post("/", (req, res, next) => { 
+    organizationdata.create( 
+        req.body,
+        (error, data) => { 
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data); 
+            }
+        }
+    );
+    organizationdata.createdAt;
+    organizationdata.updatedAt;
+    organizationdata.createdAt instanceof Date;
+});
+
+//PUT update (make sure req body doesn't have the id)
+router.put("/:id", (req, res, next) => { 
+    organizationdata.findOneAndUpdate( 
+        { _id: req.params.id }, 
+        req.body,
+        (error, data) => {
+            if (error) {
+                return next(error);
+            } else {
+                res.json(data);
+            }
+        }
+    );
+});
+
+
+// ADD DELETE API 
+router.delete("/:id",(req,res,next) =>{
+    organizationdata.findOneAndRemove({ _id: req.params.id }, (error, data) => {
+        if (error) {
+            return next(error);
+        } else {
+            res.status(200).json({
+                msg: data
+            });
+        }
+    });
+});
+
+
+module.exports = router;
