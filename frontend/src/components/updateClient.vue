20e00479a38330b99e836e1def7d0934410f7f29
Joshua Duck <jduck@cougarnet.uh.edu>
2022-11-14 19:51:59 -0600

    Adding Delete function to client page

--- frontend/src/components/updateClient.vue
+++ frontend/src/components/updateClient.vue
@@ -104,6 +104,16 @@ export default {
         });
       });
     },
+    handleClientDelete() {
+      let apiURL = import.meta.env.VITE_ROOT_API + '/primarydata/${this.id}';
+      axios.delete(apiURL, this.client).then(() => {
+        alert("Client has been deleted.");
+        this.$router.back().catch((error) =>{
+          console.log(error);
+        });
+
+      })
+    },
     addToEvent() {
       this.eventsChosen.forEach((event) => {
         let apiURL =
