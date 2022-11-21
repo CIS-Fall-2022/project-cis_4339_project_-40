b754a1a4f30c143553d7a237445a8f82a845bd47
Shivam Rana <89553377+skr20wdt@users.noreply.github.com>
2022-11-20 20:47:32 -0600

    Revision on the orgname in dashboard

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -44,11 +44,9 @@
     <div class="grow w-4/5">
       <section
         class="justify-end items-center h-24 flex"
-        style="
-          background: linear-gradient(250deg, #C8102E 70%, #efecec 50.6%);
-        "
+        style=" background: linear-gradient(250deg, #C8102E 70%, #efecec 50.6%); "
       >
-        <h1 class="mr-20 text-3xl text-white">Management Organization</h1>
+        <h1 v-for="data in allData" v-bind:key="data.id" class="mr-20 text-3xl text-white">Organization : {{ data.orgName }}</h1> 
       </section>
       <div>
         <router-view></router-view>
@@ -58,8 +56,49 @@
 </template>
 
 <script>
+
+import axios from "axios";
 export default {
   name: "App",
+  data(){//Creates array for where the JSON pulled from the API will be stored to later be called on
+    return {
+        allData:[]
+    }
+  },methods: {
+      async fetchData(){ //fetch method to pull data from the API using Axios
+        try {
+          this.error = null;
+          this.othererror = true;
+          const url = 'http://localhost:3000/organizationData/orgName'; // api call to get the name of organization 
+          const response = await axios.get(url);
+          this.allData = response.data;
+        } catch (err) { //error statements for different errors that can occur either in the backend or applicaiton itself
+          if (err.response) {
+            this.error = {
+              title: "Server Response",
+              message: err.message,
+            };
+          } else if (err.resquest) {
+            this.error = {
+              title: "Unable to Reach Server",
+              message: err.message,
+            };
+          } else {
+            this.error = {
+              title: "Application Error",
+              message: err.message,
+            };
+          }
+        }
+        this.othererror = false;
+      },
+    },
+    mounted() {
+      this.fetchData();
+    },
+  
+  
+
 };
 </script>
 
