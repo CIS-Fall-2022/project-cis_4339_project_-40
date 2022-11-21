9728a80eec65428e4bf8d7a7f5805f0b916d7fed
joshuaduck <89745365+joshuaduck@users.noreply.github.com>
2022-11-20 21:31:38 -0600

    Updated last comments for new features added

--- frontend/src/assets/barChart.vue
+++ frontend/src/assets/barChart.vue
@@ -1,19 +1,19 @@
 <template>
         <canvas ref="myChart"></canvas> <!-- references chart -->
     </template>
-
-    <script>
+    <!-- THis is a template we got from class and from Chart JS. -->
+    <script> 
     import { Chart, registerables } from 'chart.js'
     Chart.register(...registerables);
     export default {
-        props: {
+        props: { //props that will be used for labels and chart data
             label: {
                 type: Array,
             },
             chartData: {
                 type: Array,
             },
-        },
+        }, 
         async mounted() {
             console.log(this.chartData);
             await new Chart(this.$refs.myChart, {
@@ -22,7 +22,7 @@
                     labels: this.label,
                     datasets: [
                         {
-                            label: "Total Attendees",
+                            label: "Total Attendees",  //labels for the bar graph and the data that will be pushed into it from the backend.
                             backgroundColor: "rgb(189, 209, 228)",
                             data: this.chartData,
                         },
